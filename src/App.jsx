1651fd8b7f12e60bae291e24c258a90497b1088d
Santix <santiago_ivan_copaexample@hotmail.com>
2025-09-24 20:56:04 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="text-2xl font-bold mb-2">The Creative Crew</h1>
+      <p>
+        <strong>Who we are </strong>
+        <br />
+        We are a team of creatively diverse. Driven. <br />
+        Innovative individuals working in various <br />
+        locations around the world.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Photos.js
@@ -0,0 +1,12 @@
+export const Photos = [
+  {
+    image: "/images/photo1.png",
+    name: "Bill Mahoney",
+    oficio: "PRODUCT OWNER",
+  },
+  { image: "/images/photo2.png", name: "Saba Cabrera", oficio: "ART DIRECTOR" },
+  { image: "/images/photo3.png", name: "Shae Le", oficio: "TECH LEAD" },
+  { image: "/images/photo4.png", name: "Skylah Lu", oficio: "UX DESIGNER" },
+  { image: "/images/photo5.png", name: "Griff Richards", oficio: "DEVELOPER" },
+  { image: "/images/photo6.png", name: "Stan John", oficio: "DEVELOPER" },
+];
