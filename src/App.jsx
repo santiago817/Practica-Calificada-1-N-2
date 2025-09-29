d09e0efe5b27912a18492fd14558fef9a77c5b18
Santix <santiago_ivan_copaexample@hotmail.com>
2025-09-28 21:03:29 -0300

    Actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
+import { Photos } from "./components/Photos";
+import "./App.css";
 function App() {
   return (
-    <div className="app-container">
-      <h1 className="text-2xl font-bold mb-2">The Creative Crew</h1>
-      <p>
-        <strong>Who we are </strong>
-        <br />
-        We are a team of creatively diverse. Driven. <br />
-        Innovative individuals working in various <br />
-        locations around the world.
-      </p>
+    <div>
+      <div className="app-container">
+        <h1>The Creative Crew</h1>
+        <p>
+          <strong>Who we are </strong>
+          <br />
+          We are a team of creatively diverse. Driven. <br />
+          Innovative individuals working in various <br />
+          locations around the world.
+        </p>
+      </div>
+
+      
+      <div className="cards-container">
+        {Photos.map((person) => (
+          <div key={person.id}>
+            <img src={person.image} alt={person.name} className="photo" />
+            <h3 className="nombre">{person.name}</h3>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,12 +1,40 @@
+// Photos.js
+
 export const Photos = [
   {
+    id: 1,
     image: "/images/photo1.png",
     name: "Bill Mahoney",
     oficio: "PRODUCT OWNER",
   },
-  { image: "/images/photo2.png", name: "Saba Cabrera", oficio: "ART DIRECTOR" },
-  { image: "/images/photo3.png", name: "Shae Le", oficio: "TECH LEAD" },
-  { image: "/images/photo4.png", name: "Skylah Lu", oficio: "UX DESIGNER" },
-  { image: "/images/photo5.png", name: "Griff Richards", oficio: "DEVELOPER" },
-  { image: "/images/photo6.png", name: "Stan John", oficio: "DEVELOPER" },
+  {
+    id: 2,
+    image: "/images/photo2.png",
+    name: "Saba Cabrera",
+    oficio: "ART DIRECTOR",
+  },
+  {
+    id: 3,
+    image: "/images/photo3.png",
+    name: "Shae Le",
+    oficio: "TECH LEAD",
+  },
+  {
+    id: 4,
+    image: "/images/photo4.png",
+    name: "Skylah Lu",
+    oficio: "UX DESIGNER",
+  },
+  {
+    id: 5,
+    image: "/images/photo5.png",
+    name: "Griff Richards",
+    oficio: "DEVELOPER",
+  },
+  {
+    id: 6,
+    image: "/images/photo6.png",
+    name: "Stan John",
+    oficio: "DEVELOPER",
+  },
 ];
